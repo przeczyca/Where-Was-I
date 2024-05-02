dfeca14f0b34cd9c302628b588ce0621d23d0a4d
PKR <przeczyca@gmail.com>
2024-05-02 14:56:06 -0500

    update db

--- Server/server.go
+++ Server/server.go
@@ -1,25 +1,55 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"strings"
+
+	_ "github.com/lib/pq"
 )
 
 func main() {
+	db := connectToDB()
+
 	mux := http.NewServeMux()
 
-	mux.Handle("/visited", &visitedHandler{})
-	mux.Handle("/visited/", &visitedHandler{})
+	mux.Handle("/visited", &visitedHandler{db})
+	mux.Handle("/visited/", &visitedHandler{db})
 
 	http.ListenAndServe("localhost:8080", mux)
+}
+
+type VisitedLocation struct {
+	GNIS_ID string
+	Saved   bool
+	Action  string
+}
+
+func connectToDB() (db *sql.DB) {
+	var conninfo string = "postgresql://postgres:TyrantLizard@69@localhost:5432/postgres?sslmode=disable"
+
+	db, err := sql.Open("postgres", conninfo)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
+	return
 }
 
 func enableCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
 }
 
-type visitedHandler struct{}
+type visitedHandler struct {
+	db *sql.DB
+}
 
 func InternalServerErrorHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusInternalServerError)
@@ -45,7 +75,7 @@ func (h *visitedHandler) GetVisited(w http.ResponseWriter, r *http.Request) {
 func (h *visitedHandler) UpdateVisited(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("update visited called")
 
-	var visited []string
+	var visited []VisitedLocation
 
 	if err := json.NewDecoder(r.Body).Decode(&visited); err != nil {
 		fmt.Println(err)
@@ -53,7 +83,52 @@ func (h *visitedHandler) UpdateVisited(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Print(visited)
+	_, err := fmt.Println(visited)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var insertQuery strings.Builder
+	var deleteQuery strings.Builder
+
+	lastIndex := len(visited) - 1
+	for index, location := range visited {
+		if !location.Saved && location.Action == "selected" {
+			insertQuery.WriteString(fmt.Sprintf("(%s)", location.GNIS_ID))
+			if index < lastIndex {
+				insertQuery.WriteString(", ")
+			}
+		}
+	}
+
+	for index, location := range visited {
+		if location.Action == "deleted" {
+			deleteQuery.WriteString(fmt.Sprintf(location.GNIS_ID))
+			fmt.Println(index, lastIndex)
+			if index < lastIndex {
+				deleteQuery.WriteString(", ")
+			}
+		}
+	}
+
+	var fullQuery strings.Builder
+
+	if insertQuery.Len() > 0 {
+		fullQuery.WriteString("INSERT INTO visited_locations (gnis_id) VALUES ")
+		fullQuery.WriteString(insertQuery.String() + ";")
+	}
+
+	if deleteQuery.Len() > 0 {
+		fullQuery.WriteString("DELETE FROM visited_locations WHERE gnis_id IN (")
+		fullQuery.WriteString(deleteQuery.String() + ");")
+	}
+
+	fmt.Println(fullQuery.String())
+
+	_, err = h.db.Query(fullQuery.String())
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	jsonBytes, err := json.Marshal(visited)
 	if err != nil {
