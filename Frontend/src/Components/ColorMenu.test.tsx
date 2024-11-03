ebf683c07ba6dd3240c24761ba51055c4701d678
PKR <przeczyca@gmail.com>
2024-11-03 09:59:30 -0500

    First test

--- Frontend/src/Components/ColorMenu.test.tsx
+++ Frontend/src/Components/ColorMenu.test.tsx
@@ -1,7 +1,24 @@
-import { describe, it } from 'vitest'
-import { render, screen } from '@testing-library/react'
+import { describe, expect, it } from 'vitest'
+import { fireEvent, render, renderHook, screen } from '@testing-library/react'
 import ColorMenu from './ColorMenu'
-import { SelectedGNIS_ID } from '../Types';
+import { Color, ColorMenuContextInterface, SelectedGNIS_ID } from '../Types';
+import { useState } from 'react';
+import { ColorMenuContext } from '../context';
+
+let cmc: ColorMenuContextInterface;
+
+renderHook(() => {
+    const [savedColors, setSavedColors] = useState<Color[]>([{ Action: "saved", Color_ID: 1, Description: "Default", HexValue: "#123456" }]);
+    const [selectedColorID, setSelectedColorID] = useState<number>(1);
+    const [, setColorChanged] = useState<boolean>(false);
+    cmc = {
+        savedColors: savedColors,
+        setSavedColors: setSavedColors,
+        selectedColorID: selectedColorID,
+        setSelectedColorID: setSelectedColorID,
+        setColorChanged: setColorChanged,
+    }
+});
 
 const testgnisid: SelectedGNIS_ID = { GNIS_ID: "12345", Saved: true, Action: "saved", Color_ID: 1 }
 
@@ -22,8 +39,24 @@ const changeSelectionsToDefaultColorByColorID = (colorID: number) => {
 }
 
 describe("Color menu", () => {
-    it("renders closed color menu", () => {
-        render(<ColorMenu changeSelectionsToDefaultColorByColorID={changeSelectionsToDefaultColorByColorID} />);
-        screen.debug();
+    it("renders color menu closed", () => {
+        render(
+            <ColorMenuContext.Provider value={cmc}>
+                <ColorMenu changeSelectionsToDefaultColorByColorID={changeSelectionsToDefaultColorByColorID} />
+            </ColorMenuContext.Provider>
+        );
+        const closedMenu = screen.getByTestId("color-menu-closed");
+        expect(closedMenu).toBeTruthy();
+    });
+    it("color menu opens on click", () => {
+        render(
+            <ColorMenuContext.Provider value={cmc}>
+                <ColorMenu changeSelectionsToDefaultColorByColorID={changeSelectionsToDefaultColorByColorID} />
+            </ColorMenuContext.Provider>
+        );
+        let elementToTest = screen.getByTestId("color-menu-closed");
+        fireEvent.click(elementToTest);
+        elementToTest = screen.getByTestId("color-menu-open");
+        expect(elementToTest).toBeTruthy();
     });
 });
--- Frontend/vitest.config.ts
+++ Frontend/vitest.config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from 'vitest/config'
 // https://vitejs.dev/config/
 export default defineConfig({
     test: {
-        environment: "jsdom"
+        globals: true,
+        environment: "jsdom",
+        setupFiles: "/setupTests.ts"
     }
 })
